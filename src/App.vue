6ff650ee25f6048bb486622d3e981447261789c3
unknown <chandrayusuff@gmail.com>
2021-07-24 19:38:04 +0800

    project pertama

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="layout" />
+</template>
+<script>
+import DefaultLayout from "@/layout/Default";
+import LoginLayout from "@/layout/Login";
+export default {
+  name: 'App',
+  components: {
+    DefaultLayout,
+    LoginLayout,
+  },
+  data() {
+    return {
+      layout: null,
+    };
+  },
+  watch: {
+    $route(to) {
+      // set layout by route meta
+      if (to.meta.layout !== undefined) {
+        this.layout = to.meta.layout
+      } else {
+        this.layout = "DefaultLayout" // this is default layout if route meta is not set
+      }
+    },
+  },
+};
+</script>
